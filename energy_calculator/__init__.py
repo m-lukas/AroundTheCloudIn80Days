64bf6b84e261cb677968b6b70c6d740dcf3c4388
Johann Hemmann <johann.hemmann@code.berlin>
2021-11-20 19:01:26 +0100

    Catch some exceptions

--- energy_calculator/__init__.py
+++ energy_calculator/__init__.py
@@ -1,14 +1,15 @@
 import csv
-from typing import List, Set
+from typing import List, Set, Tuple
 
 
 Region = str
+GreenEnergyShare = int
 
 
 def best_server_location(regions_to_consider: Set[Region]) -> List[Region]:
     "Calculate the region with the best available energy mix."
 
-    ranking = []
+    ranking: List[Tuple[Region, GreenEnergyShare]] = []
 
     # calculate share of green energy for each region
     for region in regions_to_consider:
@@ -19,7 +20,11 @@ def best_server_location(regions_to_consider: Set[Region]) -> List[Region]:
             [a for idx, a in enumerate(latest_data) if idx in GREEN_ENERGY_SOURCES]
         )
 
-        ranking.append((region, green_energy / total_energy * 100))
+        if total_energy == 0:
+            ranking.append((region, 0))
+        else:
+            green_energy_share = green_energy / total_energy * 100
+            ranking.append((region, green_energy_share))
 
     # sort the countries by the share of renewable energy
     sorted_ranking = sorted(ranking, key=lambda x: x[1], reverse=True)
@@ -30,19 +35,23 @@ def best_server_location(regions_to_consider: Set[Region]) -> List[Region]:
 GREEN_ENERGY_SOURCES: List[int] = [0, 8, 9, 11, 12, 13, 16, 17, 19, 20]
 
 
-def data_of_region(region: Region) -> List[int]:
+def data_of_region(region: Region) -> List[str]:
     data = []
-    with open(f"data/{region}-2021-11-20.csv", newline="") as file:
-        reader = csv.reader(file)
+    try:
+        with open(f"data/{region}-2021-11-20.csv", newline="") as file:
+            reader = csv.reader(file)
 
-        # skip column names
-        reader.__next__()
+            # skip column names
+            reader.__next__()
 
-        for row in reader:
-            if "-" in row:
-                break
-            data.append(row[2:])
-    return data
+            for row in reader:
+                if "-" in row:
+                    break
+                data.append(row[2:])
+        return data
+    except FileNotFoundError:
+        print(f"Region '{region}' is not available")
+        return [[0]]
 
 
 def to_int(val: str) -> int:
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from energy_calculator import best_server_location
 
 if __name__ == "__main__":
-    print(best_server_location({"AT", "CH", "DE"}))
+    print(
+        best_server_location(
+            {"AT", "CH", "DE", "DK", "ES", "FI", "FR", "GR", "IE", "NO"}
+        )
+    )
